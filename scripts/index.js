d52b7719611fe88d47b5a721c2dd9958450cdfcb
Artem Grigorev <artem.grigorev1992@mail.ru>
2022-12-15 13:32:58 +0300

    править JS

--- scripts/index.js
+++ scripts/index.js
@@ -1,29 +1,32 @@
 const aboutPopup = document.querySelector('.popup')
 const profileEditButton = document.querySelector('.profile__edit-button');
-const aboutCloseButton = document.querySelector('.popup__close-button');
+const profileCloseButton = document.querySelector('.popup__close-button');
 const formElement = document.querySelector('.popup__form')
 const nameTitle = document.querySelector('.profile__title');
 const jobAbout = document.querySelector('.profile__about');
 const nameInput = aboutPopup.querySelector('.popup__input_text_name');
 const jobInput = aboutPopup.querySelector('.popup__input_text_job');
 
-profileEditButton.addEventListener('click', () => {
+
+function openPopup() {
   aboutPopup.classList.add('popup_opend');
   nameInput.value = nameTitle.textContent
   jobInput.value = jobAbout.textContent
-})
+}
 
-aboutCloseButton.addEventListener('click', () => {
+function closePopup() {
   aboutPopup.classList.remove('popup_opend');
-})
+}
 
 function handleFormSubmit(evt) {
   evt.preventDefault();
   nameTitle.textContent = nameInput.value;
   jobAbout.textContent = jobInput.value;
-  aboutCloseButton();
+  closePopup ();
 };
 
-formElement.addEventListener('submit', formElement);
+profileEditButton.addEventListener("click", openPopup);
+profileCloseButton.addEventListener("click", closePopup);
+formElement.addEventListener('submit', handleFormSubmit);
 
 
